4b9e6fb14de1f3f40e0a2c3c15f68b33432ae9d7
romor <romor@b79208f6-e73a-0410-af31-b51c0714e0b8>
2008-01-25 14:30:50 +0000

    - add release notes web page

--- trunk/web/releaseinfo.php
+++ trunk/web/releaseinfo.php
@@ -1,3 +1,42 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
-This page is not implemented yet.
+<head>
+ <title>BalloonRSS - Release Notes</title>
+ <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+ <meta http-equiv="content-language" content="en">
+ <meta name="author" content="Roman Morawek">
+ <meta name="description" content="Release Notes of BalloonRss.">
+ <meta name="date" content="2008-01-25T11:06:13+01:00">
+ <link rel=stylesheet type="text/css" href="/default.css">
+ <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+</head>
+
+<body>
+
+<div class="Titel"><p>BalloonRSS - A Simple RSS Aggregator using Balloon Tooltips</p></div>
+
+<h1>Release Notes</h1>
+
+<h2>Release 2.1 (January 25th, 2008)
+<ul>
+<li>add German translation
+<li>add Portuguese tanslation
+<li>multiple language support
+<li>possibility to check for available application updates
+<li>make the view rate dependant on channel priority
+<li>bug fix: avoid exception in case of RSS feed with empty description text
+</ul>
+
+<h1>Download Information</h1>
+<p>The source code as well as the released packages can be downloaded from the <a href="http://sourceforge.net/project/platformdownload.php?group_id=206266">sourceforge project download page</a>.</p>
+
+
+<hr>
+<table width="100%"><tr>
+<td><i>last update: <?php echo date ("r", filemtime($_SERVER["SCRIPT_FILENAME"]))?><br>
+Roman Morawek</i></td>
+<td align="right"><table align="right"><tr><td><a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=206266&amp;type=2" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a></td><td><a href="http://jigsaw.w3.org/css-validator/check/referer"><img border=0 width=88 height=31 SRC="http://jigsaw.w3.org/css-validator/images/vcss.gif" ALT="Valid CSS!"></a></td>
+<td><a href="http://validator.w3.org/check/referer"><img border=0 src="http://validator.w3.org/images/vh40" alt="Valid HTML 4.0!" height=31 width=88></a></td></tr></table></td>
+</tr></table>
+</body>
 </html>
