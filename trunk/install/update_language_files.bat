9b984be503b1a056819ca4f5e008c0a8c2db7ff4
romor <romor@b79208f6-e73a-0410-af31-b51c0714e0b8>
2008-02-21 20:00:27 +0000

    - update batch file to newest resxsync, which updates the language resource files

--- trunk/install/update_language_files.bat
+++ trunk/install/update_language_files.bat
@@ -2,12 +2,13 @@
 
 rem This batch file includes new keys in the application language files
 rem Author: Roman Morawek
+rem resxsync is a utility to synchronise resx files, you may obtain it here: http://www.screwturn.eu/ResxSync.ashx
 
 echo Culture:de
-resxsync ..\src\Properties\Resources.resx ..\src\Resources\strings.de.resx /v /s
+resxsync ..\src\Properties\Resources.resx ..\src\Resources\strings.de.resx /v /l
 
 echo.
 echo Culture:pt
-resxsync ..\src\Properties\Resources.resx ..\src\Resources\strings.pt.resx /v /s
+resxsync ..\src\Properties\Resources.resx ..\src\Resources\strings.pt.resx /v /l
 
 pause
